d893659385ce75b629b57119957d22422602ae42
rcbutle3 <Robin@Robins-MacBook-Air.local>
2017-08-21 15:58:52 -0400

    1st commit

---
+++ views/users.js
@@ -0,0 +1,7 @@
+users = [
+{user: 'Robin', email: "robinbutler2003@yahoo.com"}
+{user: 'Cam',  email: "cambutler2003@yahoo.com"}
+]
+
+function get User(username) {
+  return users. find functions(user)
